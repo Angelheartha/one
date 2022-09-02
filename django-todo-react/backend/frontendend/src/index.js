a885b8a277d8d7ba1698bdfcfce25e91404025af
John Smith <aisuruyamano@gamail.com>
2022-09-02 23:08:26 +0900

    Added files and changes required for deployment to heroku

--- django-todo-react/backend/frontendend/src/index.js
+++ django-todo-react/backend/frontendend/src/index.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import {render} from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
-import App from './components/App';
-import { createRoot } from "react-dom/client";
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import './bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App  from './App';
+import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const container = document.getElementById('root');
+const root = createRoot(container);
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 2500,
+  offset: '240px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
 
-root.render(
-    <BrowserRouter>
-        <App  />
-    </BrowserRouter>
-);
+const Root = () => (
+  <AlertProvider template={AlertTemplate} {...options}>
+    <App />
+  </AlertProvider>
+)
+
+render(<Root />, document.getElementById('root'))
